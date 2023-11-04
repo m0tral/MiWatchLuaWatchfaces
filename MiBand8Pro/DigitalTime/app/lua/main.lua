8526c1eb43554ef8f26ab1550a1a614a6ae3a802
m0tral <murphy@ya.ru>
2023-11-04 04:51:22 +0400

    digital time update

--- MiBand8Pro/DigitalTime/app/lua/main.lua
+++ MiBand8Pro/DigitalTime/app/lua/main.lua
@@ -25,11 +25,11 @@ local function entry()
 	
 	local pos = getItemsPosition();
 	
-    watchface.timeHourHigh =	Image(root, imgPath("num_0.bin"), pos.timeHourHigh)
-    watchface.timeHourLow =		Image(root, imgPath("num_0.bin"), pos.timeHourLow)
-    watchface.timeDelim =       Image(root, imgPath("delim.bin"), pos.timeDelim)
-    watchface.timeMinuteHigh =	Image(root, imgPath("num_0.bin"), pos.timeMinuteHigh)
-    watchface.timeMinuteLow =	Image(root, imgPath("num_0.bin"), pos.timeMinuteLow)
+    watchface.timeHourHigh =	Image(root, "num_0.bin", pos.timeHourHigh)
+    watchface.timeHourLow =		Image(root, "num_0.bin", pos.timeHourLow)
+    watchface.timeDelim =       Image(root, "delim.bin", pos.timeDelim)
+    watchface.timeMinuteHigh =	Image(root, "num_0.bin", pos.timeMinuteHigh)
+    watchface.timeMinuteLow =	Image(root, "num_0.bin", pos.timeMinuteLow)
 	
     dataman.subscribe("timeHourHigh", watchface.timeHourHigh.widget, function(obj, value)
         local src = string.format("num_%d.bin", value // 256)
@@ -51,8 +51,8 @@ local function entry()
 
     -- handle demiliter blinking
     dataman.subscribe("timeSecondLow", watchface.timeDelim.widget, function(obj, value)
-        local second = value // 256;
-        second = second & 0x01; -- take a low bit of second, odd/even
+        local second = value // 256
+        second = second & 0x01 -- take a low bit of second, odd/even
         if second == 0 then
             obj:clear_flag(lvgl.FLAG.HIDDEN)
         else
