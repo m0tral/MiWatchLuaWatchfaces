69b8409385c6eda34f086f6dc06eb56074261a7a
m0tral <murphy@ya.ru>
2023-11-04 05:11:38 +0400

    Analog Time refactored

--- MiBand8Pro/AnalogTime/app/lua/main.lua
+++ MiBand8Pro/AnalogTime/app/lua/main.lua
@@ -1,152 +1,58 @@
 local lvgl = require("lvgl")
 local dataman = require("dataman")
 
-local globalWidth = lvgl.HOR_RES()
-local globalHeight = lvgl.VER_RES()
+-- load our submodules
+require "image"
+require "root"
 
-local IMAGE_PATH = SCRIPT_PATH
-if not IMAGE_PATH then
-    IMAGE_PATH = "/"
-    print("Note image root path is set to: ", IMAGE_PATH)
-end
-
-print("IMAGE_PATH:", IMAGE_PATH)
-
-function imgPath(src)
-    return IMAGE_PATH .. src
-end
-
-local function Image(root, src, pos)
-    --- @class Image
-    local t = {} -- create new table
-
-    t.widget = root:Image { src = src }
-
-    local w, h = t.widget:get_img_size()
-    t.w = w
-    t.h = h
-    -- current state, center
-    t.pos = {
-        x = pos[1],
-        y = pos[2]
-    }
-
-    function t:getImageWidth()
-        return t.w
-    end
-
-    function t:getImageheight()
-        return t.h
-    end   
-
-    t.defaultY = pos[2]
-    t.lastState = STATE_POSITION_MID
-    t.state = STATE_POSITION_MID
-
-    t.widget:set {
-        w = w,
-        h = h,
-        x = t.pos.x,
-        y = t.pos.y
-    }
-
-    return t
-end
-
-local function HandImage(root, src, pos)
-    --- @class Image
-    local t = {} -- create new table
-
-    t.widget = root:Image { src = src }
-
-    local w, h = t.widget:get_img_size()
-    t.w = w
-    t.h = h
-    -- current state, center
-    t.pos = {
-        x = pos[1] - (w // 2),
-        y = pos[2] - (h // 2)
-    }
-
-    function t:getImageWidth()
-        return t.w
-    end
-
-    function t:getImageheight()
-        return t.h
-    end   
-
-    t.defaultY = pos[2]
-    t.lastState = STATE_POSITION_MID
-    t.state = STATE_POSITION_MID
-
-    t.widget:set {
-        w = w,
-        h = h,
-        x = t.pos.x,
-        y = t.pos.y
-    }
-
-    return t
-end
-
-local function createRoot()
-    local property = {
-        w = globalWidth,
-        h = globalHeight,
-        bg_color = 0,
-        bg_opa = lvgl.OPA(100),
-        border_width = 0,
-        pad_all = 0
-    }
-
-    scr = lvgl.Object(nil, property)
-    scr:clear_flag(lvgl.FLAG.SCROLLABLE)
-    return scr
-end
-
-local function calculateDefaultPosition()
+local function getItemsPosition()
     
-	local imgInitialPosition = {
+	local imgPosition = {
         background = { 0, 72 },
         timeHour =	 { 168, 240 },
         timeMinute = { 168, 240 },
         timeSecond = { 168, 240 },
     }
 
-    return imgInitialPosition
+    return imgPosition
 end
 
+-- prepare main watchface fuction
 local function entry()
+
     local root = createRoot()
 	
 	local watchface = {}
 	
-	local pos = calculateDefaultPosition();
+    -- setup watchface elements positions
+	local pos = getItemsPosition()
 	
-    watchface.background = Image(root, imgPath("img_0001.bin"), pos.background)
-    watchface.timeHour =   HandImage(root, imgPath("arrHour.bin"), pos.timeHour)
-    watchface.timeMinute = HandImage(root, imgPath("arrMinute.bin"), pos.timeMinute)
-    watchface.timeSecond = HandImage(root, imgPath("arrSecond.bin"), pos.timeSecond)
+    -- create watchface elements
+    watchface.background = Image(root, "img_0001.bin", pos.background)
+    watchface.timeHour =   HandImage(root, "arrHour.bin", pos.timeHour)
+    watchface.timeMinute = HandImage(root, "arrMinute.bin", pos.timeMinute)
+    watchface.timeSecond = HandImage(root, "arrSecond.bin", pos.timeSecond)
 	
+    -- attach events and setup update behaviour
     dataman.subscribe("timeHour", watchface.timeHour.widget, function(obj, value)
 		local hour = value // 0x100
-		local hourPos = 7200 // 24 * hour;
+		local hourPos = 7200 // 24 * hour
 		obj:set { angle = hourPos }
     end)
 
     dataman.subscribe("timeMinute", watchface.timeMinute.widget, function(obj, value)
 		local min = value // 0x100
-		local minPos = 3600 // 60 * min;
+		local minPos = 3600 // 60 * min
         obj:set { angle = minPos }
     end)
 
     dataman.subscribe("timeSecond", watchface.timeSecond.widget, function(obj, value)
 		local sec = value // 0x100
-		local secPos = 3600 // 60 * sec;
+		local secPos = 3600 // 60 * sec
         obj:set { angle = secPos }
     end)
 
 end
 
+-- execute watchface function
 entry()
